b1083ab842ce4a73ac66a83038483c74bb8a251d
Sai Gudigundla <saip106@msn.com>
2018-05-24 00:59:40 -0400

    added more functionality

--- UnitTesting/UnitTesting.Sample/PatientRepository.cs
+++ UnitTesting/UnitTesting.Sample/PatientRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 
@@ -11,6 +12,21 @@ namespace UnitTesting.Sample
             var patientsJson = File.ReadAllText(@"c:\temp\patients.json");
             var patients = JsonConvert.DeserializeObject<Patient[]>(patientsJson);
             return patients.Single(x => x.Id == id);
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            var patientsJson = File.ReadAllText(@"c:\temp\patients.json");
+            var patients = JsonConvert.DeserializeObject<List<Patient>>(patientsJson);
+            patients.Add(patient);
+            var updatedPatientJson = JsonConvert.SerializeObject(patients);
+            File.WriteAllText(@"c:\temp\patients.json", updatedPatientJson);
+        }
+
+        public Patient[] GetAll()
+        {
+            var patientsJson = File.ReadAllText(@"c:\temp\patients.json");
+            return JsonConvert.DeserializeObject<Patient[]>(patientsJson);
         }
     }
 }
